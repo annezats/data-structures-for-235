50d82bb9af578cb8cf5160f2fe2ba5939c9619de
Anne Zats <anne.zats70@myhunter.cuny.edu>
2020-04-14 23:49:41 -0400

    done

--- trees/tree.cpp
+++ trees/tree.cpp
@@ -9,21 +9,33 @@ void tree::insert( int data){
 
 }
 
-std::string tree::get_debug_string(){
-  if (root == nullptr)
+std::string tree::make_debug_string(Node * root){ //makes debugger starting at any node
+  if (root == nullptr){
     return " ";
-  else
-    return std::to_string(root->getData());
+  }
+  else if (root->getLeft()==nullptr && root->getRight()==nullptr){
+      return std::to_string(root->getData());
+    }
+    //else return "("+ std::to_string(root->getData()) + "-->" + make_debug_string(root->getLeft())+ ","+ make_debug_string(root->getRight())+ ")";
+    else return  std::to_string(root->getData()) +"-->" +  "("+make_debug_string(root->getLeft())+ "),("+ make_debug_string(root->getRight())+ ")";
+    //the line in the comment returns the same correct thing but formatted differently (more confusingly)
+}
+
+std::string tree::get_debug_string(){
+  return make_debug_string(root); //executes the maker with specifically the root of this tree
 }
 
 void tree::setup(){
   Node *n = new Node(20);
   Node *m = new Node(30);
-  Node *p = new Node(40);
+  Node *p = new Node(50);
   Node *s = new Node(10);
+  Node *t = new Node(40);
+  Node *u = new Node(60);
   root = s;
   root->setLeft(n);
   root->setRight(p);
   root->getLeft()->setLeft(m);
-
+  root->getLeft()->setRight(t);
+  root->getRight()->setLeft(u);
 }
--- trees/tree.h
+++ trees/tree.h
@@ -5,6 +5,7 @@
 class tree{
 public:
   std::string get_debug_string();
+  std::string make_debug_string(Node * root);
   void insert(int data);
   tree();
   void setup(); //hardcodes a tree for setup
